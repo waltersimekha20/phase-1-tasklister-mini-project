7e63dbb927fe55d094060cef85845b4f76f02b0d
waltersimekha <waltersimekha@gmail.com>
2023-03-27 13:37:11 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener("DOMContentLoaded", () => {
+  addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const chore = document.querySelector("#new-task-description").value;
+
+    // Create a new task element and add it to the list
+    const newTask = document.createElement("li");
+    newTask.textContent = chore;
+    document.querySelector("#tasks").appendChild(newTask);
+
+    chore.value = "";  // Reset the input field to an empty string
+  });
