50f5b97f74ba8f94afce9ccb1ee91dc16166b6cc
waltersimekha <waltersimekha@gmail.com>
2023-04-02 16:30:33 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
-document.addEventListener("DOMContentLoaded", () => {
-  addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const chore = document.querySelector("#new-task-description").value;
-
-    // Create a new task element and add it to the list
-    const newTask = document.createElement("li");
-    newTask.textContent = chore;
-    document.querySelector("#tasks").appendChild(newTask);
-
-    chore.value = "";  // Reset the input field to an empty string
-  });
+  const formito=document.querySelector("form");
+  formito.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    buildMyTaskito(e.target.task.value);
+    formito.reset();
+  })
+  })
+  function buildMyTaskito(task){
+    const btn=document.createElement('button');
+    btn.textContent=' X'
+    btn.addEventListener('click',deleting)
+    const p=document.createElement("p");
+    p.textContent= `${task} `;
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector("#list").appendChild(p);
+  }
+  function deleting(e){
+    e.target.parentNode.remove();
+  };
+  document.addEventListener('DOMContentLoaded',()=>{
+    const formato=document.querySelector("#dates");
+    formato.addEventListener('submit', (e)=>{
+      e.preventDefault();
+      buildDate(e.target.dating.value);
+      formato.reset();
+   })
+  })
+  function buildDate(dateInfo){
+    const btn=document.createElement('button');
+    btn.textContent="done";
+    btn.addEventListener('click',deleto)
+    const p=document.createElement('p');
+    p.textContent=dateInfo
+    p.appendChild(btn)
+    console.log(p)
+    document.querySelector("#listing").appendChild(p);
+  }
+  function deleto(e){
+    e.target.parentNode.remove();
+  };
+  
+  
